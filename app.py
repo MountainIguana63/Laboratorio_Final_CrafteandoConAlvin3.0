8142000eb8c30ea7e063d1c2f163d6b53504908f
jesswiz <jess.kgonzalezc@gmail.com>
2026-07-14 16:07:26 -0600

    Rename delete endpoint and update function call

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ def modificar_libro(libro_id):
     # TODO: comprobar existencia, leer cambios y actualizar.
     return jsonify({"mensaje": "Endpoint pendiente"}), 501
 
-@app.delete("/libros/<int:libro_id>")
-def borrar_libro(libro_id):
-    # TODO: comprobar existencia, eliminar y responder correctamente.
-    return jsonify({"mensaje": "Endpoint pendiente"}), 501
+@app.delete("/libros/<int:id>")
+def eliminar(id):
+    resultado = eliminar_libro(id)
+    return jsonify(resultado), 200
 
 @app.errorhandler(Exception)
 def manejar_error(error):
